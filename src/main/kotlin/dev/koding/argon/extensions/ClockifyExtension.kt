59e927ffddc8aa02536aa3fad7bb1a6b8906cd28
Koding Dev <kodingdev@pm.me>
2022-01-19 01:50:03 +1100

    feat: Added proper currency formatting & total time

--- src/main/kotlin/dev/koding/argon/extensions/ClockifyExtension.kt
+++ src/main/kotlin/dev/koding/argon/extensions/ClockifyExtension.kt
@@ -4,9 +4,9 @@ import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.types.respondPublic
 import dev.koding.argon.data.clockify.ClockifyHelper
+import dev.koding.argon.util.asCurrency
 import dev.koding.argon.util.asMonthDisplay
 import dev.koding.argon.util.formatElapsedTime
-import dev.koding.argon.util.round
 import dev.kord.common.Color
 import dev.kord.rest.builder.message.create.embed
 import java.util.*
@@ -30,22 +30,22 @@ class ClockifyExtension(override val name: String = "Clockify") : Extension() {
                             field {
                                 val workloadSummary = data.joinToString(separator = "\n") {
                                     "`${it.project}`: ${it.description} (${it.time.formatElapsedTime()})" +
-                                            " - $${it.price.round(2)} USD"
+                                            " - ${it.price.asCurrency()}"
                                 }
-                                val totalEarned = data.sumOf { it.price }.round(2)
+                                val totalEarned = data.sumOf { it.price }.asCurrency()
                                 val totalTime = data.sumOf { it.time }.formatElapsedTime()
 
                                 name = client
-                                value = "$workloadSummary\nTotal: **$$totalEarned USD** - **$totalTime**"
+                                value = "$workloadSummary\nTotal: **$totalEarned** - **$totalTime**"
                             }
                         }
 
                         field {
+                            val totalEarned = report.values.flatten().sumOf { it.price }.asCurrency()
+                            val totalTime = report.values.flatten().sumOf { it.time }.formatElapsedTime()
+
                             name = "Total"
-                            value =
-                                "**$${
-                                    report.values.flatten().sumOf { it.price }.round(2)
-                                } USD** in ${Date().asMonthDisplay()}"
+                            value = "**$totalEarned** from $totalTime in ${Date().asMonthDisplay()}"
                         }
 
                         footer {
--- src/main/kotlin/dev/koding/argon/util/NumberUtil.kt
+++ src/main/kotlin/dev/koding/argon/util/NumberUtil.kt
@@ -1,7 +1,12 @@
 package dev.koding.argon.util
 
+import java.text.NumberFormat
+import java.util.*
+
 fun Double.round(decimals: Int): Double {
     var multiplier = 1.0
     repeat(decimals) { multiplier *= 10 }
     return kotlin.math.round(this * multiplier) / multiplier
 }
+
+fun Double.asCurrency(): String = NumberFormat.getCurrencyInstance(Locale.US).format(this)
